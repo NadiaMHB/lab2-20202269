233121e38b886b3d9ba00a5254ab6c49002ae170
NadiaMHB <nhb_02@hotmail.com>
2024-08-29 20:51:49 -0500

    laboratorio

---
+++ src/main/java/org/example/lab220202269/Lab220202269Application.java
@@ -0,0 +1,13 @@
+package org.example.lab220202269;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab220202269Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab220202269Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/lab220202269/Lab220202269ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lab220202269;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab220202269ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
